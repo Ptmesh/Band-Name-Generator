1b79d0bbfeb9cce8a31437b4874558ea9f4c6b3b
Prathmesh Takalkar <takalkarprathmesh@gmail.com>
2024-03-01 15:00:05 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to Band Name Generator - By Ptmesh😸")
+city = input("Which city did you grew up in?\n")
+nickname = input("What is the name of your pet?\n")
+bandName = city + nickname
+print('Your new band name could be.. \n' + bandName)
